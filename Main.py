28cc8dbdea5f431ae15bb12ce92d2a33931ae6b3
Batuhan Mikail Demir <batuhand960@gmail.com>
2024-06-25 21:04:35 +0300

    Click action added

--- Main.py
+++ Main.py
@@ -28,28 +28,45 @@ timer_turtle.goto(0, 300)
 timer_turtle.write("Time: 30", align="center", font=("Arial", 32, "normal"))
 
 def update_score(new_score):
-    score_turtle.clear
-    score_turtle.write(f"Time: {new_score}", align="center", font=("Arial", 32, "normal"))
+    score_turtle.clear()
+    score_turtle.write(f"Score: {new_score}", align="center", font=("Arial", 32, "normal"))
 
 def update_timer(time_left):
     timer_turtle.clear()
     timer_turtle.write(f"Time: {time_left}", align="center", font=("Arial", 32, "normal"))
+
+def end_timer():
+    timer_turtle.clear()
+    timer_turtle.hideturtle()
+    timer_turtle.penup()
+    timer_turtle.goto(0, 300)
+    timer_turtle.write("Game Over!", align="center", font=("Arial", 32, "normal"))
 
 def random_turtle(): # Kaplumbağanın rastgele konumlandırılması için 
     turtle_object.penup()
     turtle_object.speed(0)
     turtle_object.goto(randint(-255, 255), randint(-255, 255))  
 
-start_time = 30
-time_left = start_time
+def increase_score(x,y):
+    score_turtle.clear
+    global new_score
+    new_score += 1
+    update_score(new_score)
 
-while start_time > 0:
+turtle_object.onclick(increase_score)
+
+time_left = 30
+new_score = 0
+
+while time_left > 0:
     turtle_object.hideturtle()  
-    time.sleep(1)  
+    time.sleep(0.3)  
     random_turtle() 
     turtle_object.showturtle()  
-    time.sleep(1)  
+    time.sleep(0.3)  
     time_left -= 1
     update_timer(time_left)
 
+turtle_object.hideturtle()
+end_timer()
 turtle.done()
