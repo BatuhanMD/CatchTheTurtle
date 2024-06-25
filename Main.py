fcebf10e229377ffd724d4b64a675c23f8e2db9e
Batuhan Mikail Demir <batuhand960@gmail.com>
2024-06-25 20:29:24 +0300

    Random turtle location added

--- Main.py
+++ Main.py
@@ -1,24 +1,28 @@
 import turtle
 from random import randint
+import time
 
-#Game board properties
+# Game board properties
 game_board = turtle.Screen()
 game_board.bgcolor("Light Gray")
 game_board.title("Catch The Turtle")
 
-#Turtle properties
+# Turtle properties
 turtle_object = turtle.Turtle()
 turtle_object.shape("turtle")
 turtle_object.color("Green")
 
 def random_turtle():
-    turtle_object.speed("slow")
     turtle_object.penup()
-    turtle_object.goto(randint(-255,0),randint(0,255))
-i = 0
-while (i < 5 ):
-    random_turtle()
-    i += 1
+    turtle_object.goto(randint(-255, 255), randint(-255, 255))  
 
+i = 0
+while i < 5:
+    turtle_object.hideturtle()  # Kaplumbağayı gizle
+    time.sleep(1)  # 1 saniye bekle
+    random_turtle()  # Kaplumbağayı rastgele bir konuma taşı
+    turtle_object.showturtle()  # Kaplumbağayı göster
+    time.sleep(1)  # 1 saniye bekle
+    i += 1
 
 turtle.done()
